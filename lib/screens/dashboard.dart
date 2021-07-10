db2cd94fa7162bfe9ef3ed226a867204a9697ea0
Sandesh Sharma <iammandesh@gmail.com>
2021-07-10 19:11:26 +0545

    Student Dashboard UI

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -4,6 +4,7 @@ import 'package:smartschool_mobile/models/studentSubjects.dart';
 import 'package:smartschool_mobile/services/auth.dart';
 import 'package:smartschool_mobile/services/services.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:smartschool_mobile/constants.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -30,101 +31,149 @@ class _DashboardPageState extends State<DashboardPage> {
     Services.name = _auth.getName;
     // Services.isTeacher = _auth.isTeacher;
     return Scaffold(
+      // drawer: Drawer(),
       backgroundColor: Colors.transparent,
       appBar: AppBar(
-        actions: [TextButton(onPressed: () {}, child: Text('Logout'))],
+        // centerTitle: true,
+        title: Text(
+          _auth.getName ?? "",
+          style: kBodyText,
+        ),
+        actions: [
+          TextButton(
+              onPressed: () {
+                _auth.signOut();
+              },
+              child: Text('Logout'))
+        ],
         backgroundColor: Colors.transparent,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/Hackathonbg.png"),
-            fit: BoxFit.cover,
+      body: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/Hackathonbg.png"),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-        child: Expanded(
-          child: FutureBuilder(
-            initialData: [],
-            future: getStudentSubjectsList(),
-            builder: (context, AsyncSnapshot<List<dynamic>> snapshot) =>
-                snapshot.hasData
-                    ? StaggeredGridView.countBuilder(
-                        padding: EdgeInsets.all(10),
-                        crossAxisCount: 1,
-                        itemCount: snapshot.data!.length,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
-                            child: InkWell(
-                              onTap: () {
-                                // Navigator.push(
-                                //     context,
-                                //     MaterialPageRoute(
-                                //         builder: (BuildContext context) =>
-                                //             DetailsScreen(
-                                //                 snapshot.data[index])));
-                              },
-                              child: Container(
-                                padding: EdgeInsets.all(5),
-                                height: index.isOdd ? 80 : 80,
-                                decoration: BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.blueGrey,
-                                      offset: const Offset(
-                                        5.0,
-                                        5.0,
-                                      ),
-                                      blurRadius: 10.0,
-                                      spreadRadius: 2.0,
-                                    ),
-                                  ],
-                                  color: Color(0xffF5F5DC),
-                                  borderRadius: BorderRadius.circular(16),
-                                ),
-                                child: Center(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      Text(
-                                        snapshot.data![index].name ?? "Hello",
-                                        // "",
-                                        style: TextStyle(
-                                            fontSize: 15,
-                                            color: Color(0xFF0D1333),
-                                            fontWeight: FontWeight.normal),
-                                      ),
-                                      SizedBox(
-                                        height: 10,
-                                      ),
-                                      Text(
-                                        // 'Credits : ${snapshot.data[index].credits}',
-                                        "",
-                                        style: TextStyle(
-                                          color:
-                                              Color(0xFF0D1333).withOpacity(.5),
+        Center(
+          child: Container(
+            // decoration: BoxDecoration(
+            //   image: DecorationImage(
+            //     image: AssetImage("assets/images/Hackathonbg.png"),
+            //     fit: BoxFit.cover,
+            //   ),
+            // ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Text(
+                  "Subjects",
+                  style: kHeadline,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                FutureBuilder(
+                  initialData: [],
+                  future: getStudentSubjectsList(),
+                  builder: (context, AsyncSnapshot<List<dynamic>> snapshot) =>
+                      snapshot.hasData
+                          ? Expanded(
+                              child: StaggeredGridView.countBuilder(
+                                padding: EdgeInsets.all(20),
+                                crossAxisCount: 1,
+                                itemCount: snapshot.data!.length,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 15,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                                    child: InkWell(
+                                      onTap: () {
+                                        // Navigator.push(
+                                        //     context,
+                                        //     MaterialPageRoute(
+                                        //         builder: (BuildContext context) =>
+                                        //             DetailsScreen(
+                                        //                 snapshot.data[index])));
+                                      },
+                                      child: Container(
+                                        padding: EdgeInsets.all(5),
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.blueGrey,
+                                              offset: const Offset(
+                                                5.0,
+                                                5.0,
+                                              ),
+                                              blurRadius: 5.0,
+                                              spreadRadius: 1.0,
+                                            ),
+                                          ],
+                                          color: Color(0xffF5F5DC),
+                                          borderRadius:
+                                              BorderRadius.circular(16),
                                         ),
-                                      )
-                                    ],
-                                  ),
-                                ),
+                                        child: Center(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              Text(
+                                                snapshot.data![index].name ??
+                                                    "Hello",
+                                                // "",
+                                                style: TextStyle(
+                                                    fontSize: 15,
+                                                    color: Color(0xFF0D1333),
+                                                    fontWeight:
+                                                        FontWeight.normal),
+                                                overflow: TextOverflow.clip,
+                                              ),
+                                              // SizedBox(
+                                              //   height: 10,
+                                              // ),
+                                              // Text(
+                                              //   // 'Credits : ${snapshot.data[index].credits}',
+                                              //   "",
+                                              //   style: TextStyle(
+                                              //     color: Color(0xFF0D1333)
+                                              //         .withOpacity(.5),
+                                              //   ),
+                                              // )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                staggeredTileBuilder: (index) =>
+                                    StaggeredTile.fit(1),
                               ),
-                            ),
-                          );
-                        },
-                        staggeredTileBuilder: (index) => StaggeredTile.fit(1),
-                      )
-                    : Center(
-                        child: CircularProgressIndicator(
-                        backgroundColor: Colors.green,
-                      )),
+                            )
+                          : Center(
+                              child: CircularProgressIndicator(
+                              backgroundColor: Colors.green,
+                            )),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
