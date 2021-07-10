2a86e24d72a1e4070b180e05cd2e7339791b8545
Sandesh Sharma <iammandesh@gmail.com>
2021-07-10 22:21:43 +0545

    Recording/Resources tabbarview implemented

--- lib/screens/recordingList.dart
+++ lib/screens/recordingList.dart
@@ -12,10 +12,34 @@ class RecordingsPage extends StatefulWidget {
   _RecordingsPageState createState() => _RecordingsPageState();
 }
 
-class _RecordingsPageState extends State<RecordingsPage> {
+class _RecordingsPageState extends State<RecordingsPage>
+    with SingleTickerProviderStateMixin {
   Future<dynamic> getChapterDetails(String? id) async {
     final chapterDetails = await Services.getChapterDetails(id);
     return chapterDetails!;
+  }
+
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: 'Recordings'),
+    Tab(
+      text: 'Resources',
+    ),
+  ];
+  TabController? _tabController;
+
+  int _initialIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+        vsync: this, length: myTabs.length, initialIndex: _initialIndex);
+  }
+
+  @override
+  void dispose() {
+    _tabController!.dispose();
+    super.dispose();
   }
 
   @override
@@ -35,116 +59,195 @@ class _RecordingsPageState extends State<RecordingsPage> {
             ),
           ),
         ),
-        Center(
-          child: Container(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                ),
-                Text(
-                  "Recordings",
-                  style: kHeadline,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                FutureBuilder(
-                  future: getChapterDetails(widget.id),
-                  builder: (context, AsyncSnapshot<dynamic> snapshot) =>
-                      snapshot.hasData
-                          ? Expanded(
-                              child: StaggeredGridView.countBuilder(
-                                padding: EdgeInsets.all(20),
-                                crossAxisCount: 1,
-                                itemCount: snapshot.data!.recordings.length,
-                                crossAxisSpacing: 10,
-                                mainAxisSpacing: 15,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding:
-                                        const EdgeInsets.fromLTRB(15, 5, 15, 5),
-                                    child: InkWell(
-                                      onTap: () {
-                                        // Navigator.push(
-                                        //     context,
-                                        //     MaterialPageRoute(
-                                        //         builder: (BuildContext context) =>
-                                        //             DetailsScreen(
-                                        //                 snapshot.data[index])));
-                                      },
-                                      child: Container(
-                                        padding: EdgeInsets.all(5),
-                                        height: 50,
-                                        decoration: BoxDecoration(
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: Colors.blueGrey,
-                                              offset: const Offset(
-                                                5.0,
-                                                5.0,
-                                              ),
-                                              blurRadius: 5.0,
-                                              spreadRadius: 1.0,
-                                            ),
-                                          ],
-                                          color: Color(0xffF5F5DC),
-                                          borderRadius:
-                                              BorderRadius.circular(16),
-                                        ),
-                                        child: Center(
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: <Widget>[
-                                              Text(
-                                                snapshot.data!.recordings[index]
-                                                        .title ??
-                                                    "Hello",
-                                                // "",
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    color: Color(0xFF0D1333),
-                                                    fontWeight:
-                                                        FontWeight.normal),
-                                                overflow: TextOverflow.clip,
-                                                maxLines: 1,
-                                              ),
-                                              // SizedBox(
-                                              //   height: 10,
-                                              // ),
-                                              // Text(
-                                              //   // 'Credits : ${snapshot.data[index].credits}',
-                                              //   "",
-                                              //   style: TextStyle(
-                                              //     color: Color(0xFF0D1333)
-                                              //         .withOpacity(.5),
-                                              //   ),
-                                              // )
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                },
-                                staggeredTileBuilder: (index) =>
-                                    StaggeredTile.fit(1),
-                              ),
-                            )
-                          : Center(
-                              child: CircularProgressIndicator(
-                              backgroundColor: Colors.green,
-                            )),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                ),
-              ],
+        Column(
+          children: [
+            Container(
+              decoration: new BoxDecoration(color: Colors.black),
+              child: new TabBar(
+                indicatorPadding: EdgeInsets.all(5),
+                indicatorWeight: 3.0,
+                indicatorColor: Colors.white,
+                labelColor: Colors.white,
+                controller: _tabController,
+                tabs: myTabs,
+              ),
             ),
-          ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+              // height: 10,
+            ),
+            // Text(
+            //   "Recordings",
+            //   style: kHeadline,
+            // ),
+            // SizedBox(
+            //   height: 20,
+            // ),
+
+            Expanded(
+              child: FutureBuilder(
+                future: getChapterDetails(widget.id),
+                builder: (context, AsyncSnapshot<dynamic> snapshot) => snapshot
+                        .hasData
+                    ? TabBarView(controller: _tabController, children: [
+                        StaggeredGridView.countBuilder(
+                          padding: EdgeInsets.all(15),
+                          crossAxisCount: 1,
+                          itemCount: snapshot.data!.recordings.length,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 15,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                              child: InkWell(
+                                onTap: () {
+                                  // Navigator.push(
+                                  //     context,
+                                  //     MaterialPageRoute(
+                                  //         builder: (BuildContext context) =>
+                                  //             DetailsScreen(
+                                  //                 snapshot.data[index])));
+                                },
+                                child: Container(
+                                  padding: EdgeInsets.all(5),
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.blueGrey,
+                                        offset: const Offset(
+                                          5.0,
+                                          5.0,
+                                        ),
+                                        blurRadius: 5.0,
+                                        spreadRadius: 1.0,
+                                      ),
+                                    ],
+                                    color: Color(0xffF5F5DC),
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                  child: Center(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          snapshot.data!.recordings[index]
+                                                  .title ??
+                                              "Hello",
+                                          // "",
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: Color(0xFF0D1333),
+                                              fontWeight: FontWeight.normal),
+                                          overflow: TextOverflow.clip,
+                                          maxLines: 1,
+                                        ),
+                                        // SizedBox(
+                                        //   height: 10,
+                                        // ),
+                                        // Text(
+                                        //   // 'Credits : ${snapshot.data[index].credits}',
+                                        //   "",
+                                        //   style: TextStyle(
+                                        //     color: Color(0xFF0D1333)
+                                        //         .withOpacity(.5),
+                                        //   ),
+                                        // )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                        ),
+                        StaggeredGridView.countBuilder(
+                          padding: EdgeInsets.all(15),
+                          crossAxisCount: 1,
+                          itemCount: snapshot.data!.recordings.length,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 15,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                              child: InkWell(
+                                onTap: () {
+                                  // Navigator.push(
+                                  //     context,
+                                  //     MaterialPageRoute(
+                                  //         builder: (BuildContext context) =>
+                                  //             DetailsScreen(
+                                  //                 snapshot.data[index])));
+                                },
+                                child: Container(
+                                  padding: EdgeInsets.all(5),
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.blueGrey,
+                                        offset: const Offset(
+                                          5.0,
+                                          5.0,
+                                        ),
+                                        blurRadius: 5.0,
+                                        spreadRadius: 1.0,
+                                      ),
+                                    ],
+                                    color: Color(0xffF5F5DC),
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                  child: Center(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          snapshot.data!.recordings[index]
+                                                  .title ??
+                                              "Hello",
+                                          // "",
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: Color(0xFF0D1333),
+                                              fontWeight: FontWeight.normal),
+                                          overflow: TextOverflow.clip,
+                                          maxLines: 1,
+                                        ),
+                                        // SizedBox(
+                                        //   height: 10,
+                                        // ),
+                                        // Text(
+                                        //   // 'Credits : ${snapshot.data[index].credits}',
+                                        //   "",
+                                        //   style: TextStyle(
+                                        //     color: Color(0xFF0D1333)
+                                        //         .withOpacity(.5),
+                                        //   ),
+                                        // )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                        ),
+                      ])
+                    : Center(
+                        child: CircularProgressIndicator(
+                        backgroundColor: Colors.green,
+                      )),
+              ),
+            ),
+          ],
         ),
       ]),
     );
