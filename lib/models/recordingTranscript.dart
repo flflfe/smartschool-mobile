d89fdcca1dc1fa48f59ff6116f353a988d2f6cfd
Sandesh Sharma <iammandesh@gmail.com>
2021-07-11 08:32:20 +0545

    transcriptModel created

---
+++ lib/models/recordingTranscript.dart
@@ -0,0 +1,133 @@
+// To parse this JSON data, do
+//
+//     final transcript = transcriptFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Transcript> transcriptFromJson(String str) => List<Transcript>.from(json
+    .decode(str)['recording']['transcript']
+    .map((x) => Transcript.fromJson(x)));
+
+class Transcript {
+  Transcript({
+    this.from,
+    this.sentiment,
+    this.id,
+    this.text,
+    this.startTime,
+    this.endTime,
+    this.phrases,
+    this.mId,
+    this.v,
+  });
+
+  From? from;
+  Sentiment? sentiment;
+  String? id;
+  String? text;
+  int? startTime;
+  int? endTime;
+  List<Phrase>? phrases;
+  int? mId;
+  int? v;
+
+  factory Transcript.fromJson(Map<String, dynamic> json) => Transcript(
+        from: From.fromJson(json["from"]),
+        sentiment: Sentiment.fromJson(json["sentiment"]),
+        id: json["_id"],
+        text: json["text"],
+        startTime: json["startTime"],
+        endTime: json["endTime"],
+        phrases:
+            List<Phrase>.from(json["phrases"].map((x) => Phrase.fromJson(x))),
+        mId: json["m_id"],
+        v: json["__v"],
+      );
+}
+
+class From {
+  From({
+    this.id,
+    this.name,
+  });
+
+  String? id;
+  Name? name;
+
+  factory From.fromJson(Map<String, dynamic> json) => From(
+        id: json["id"],
+        name: nameValues.map![json["name"]],
+      );
+}
+
+enum Name { SPEAKER_2, SPEAKER_1 }
+
+final nameValues =
+    EnumValues({"Speaker 1": Name.SPEAKER_1, "Speaker 2": Name.SPEAKER_2});
+
+class Phrase {
+  Phrase({
+    this.id,
+    this.type,
+    this.text,
+  });
+
+  String? id;
+  String? type;
+  String? text;
+
+  factory Phrase.fromJson(Map<String, dynamic> json) => Phrase(
+        id: json["_id"],
+        type: json["type"],
+        text: json["text"],
+      );
+}
+
+class Sentiment {
+  Sentiment({
+    this.polarity,
+    this.suggested,
+  });
+
+  Polarity? polarity;
+  Suggested? suggested;
+
+  factory Sentiment.fromJson(Map<String, dynamic> json) => Sentiment(
+        polarity: Polarity.fromJson(json["polarity"]),
+        suggested: suggestedValues.map![json["suggested"]],
+      );
+}
+
+class Polarity {
+  Polarity({
+    this.score,
+  });
+
+  double? score;
+
+  factory Polarity.fromJson(Map<String, dynamic> json) => Polarity(
+        score: json["score"].toDouble(),
+      );
+}
+
+enum Suggested { POSITIVE, NEUTRAL, NEGATIVE }
+
+final suggestedValues = EnumValues({
+  "negative": Suggested.NEGATIVE,
+  "neutral": Suggested.NEUTRAL,
+  "positive": Suggested.POSITIVE
+});
+
+class EnumValues<T> {
+  Map<String, T>? map;
+  Map<T, String>? reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    if (reverseMap == null) {
+      reverseMap = map!.map((k, v) => new MapEntry(v, k));
+    }
+    return reverseMap!;
+  }
+}
